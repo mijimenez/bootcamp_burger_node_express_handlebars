3132de380250e16643c9a423d50a22e7a7b4f064
Madeline Jimenez <madelineijimenez@gmail.com>
2020-02-12 07:54:22 -0500

    Create/POST function

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Tex-Mex Burger');
+INSERT INTO burgers (burger_name) VALUES ('Feta Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Italian Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Egg Burger');
