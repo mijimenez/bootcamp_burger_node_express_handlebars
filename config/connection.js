b017e57eb63df8534ca571c0ea73ebe1ede0bfaf
Madeline Jimenez <madelineijimenez@gmail.com>
2020-02-15 20:20:47 -0500

    Add JAWSDB_URL

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,17 @@ var mysql = require("mysql");
 
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
